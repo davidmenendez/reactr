84926c42d3e74211faa5548d4e3b87cac6a6ee57
David Menendez <damenend@gmail.com>
2016-06-06 12:37:16 -0500

    added sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var babelify = require('babelify');
 var browserify = require('browserify');
 var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
@@ -14,4 +16,16 @@ gulp.task('build', function () {
   .pipe(buffer())
   .pipe(uglify())
   .pipe(gulp.dest('public'));
+});
+
+gulp.task('styles', function(){
+  gulp.src('src/sass/**/*.scss')
+  .pipe(sass(
+    {outputStyle: 'expanded'}
+    ).on('error', sass.logError))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.dest('./public'));
 });
