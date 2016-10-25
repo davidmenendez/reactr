e63178978ca6bd9751e627f29cc1060c1d65cd7d
David Menendez <damenend@gmail.com>
2016-10-24 23:37:39 -0500

    oops

--- creds.sh
+++
@@ -1,4 +0,0 @@
-export consumer_key="ZjrgkhzgF2lGbvj7KL06SBXhH"
-export consumer_secret="Qi85jeVAbjQWizT4hhAZrJfaYZt8oQyJCuJv9g2V0TIKTVvG3Z"
-export access_token_key="3063435388-YIFZdLLiuoBhHYcKh8i9EDgAyeXb1KV449SAGMi"
-export access_token_secret="J42iMHfQQoJexVTEBdJ7KiFkj8xMQfBTNDlwKbSYRA4hW"
