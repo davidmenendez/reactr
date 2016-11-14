1a8347784987d075cfcca3a6464e92739ee47534
David Menendez <damenend@gmail.com>
2016-11-14 14:48:24 -0600

    added morgan logger

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ var bodyParser = require('body-parser');
 var config = require('./config.js');
 var fakeResults = require('./fake_results.json');
 var compression = require('compression');
+var logger = require('morgan');
 var useFakeResults = process.env.FAKERESULTS ? true : false;
 var app = express();
 
 app.use(compression());
+app.use(logger('dev'));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({secret: 'mrbig500', resave: false, saveUninitialized: true}));
 
 var twitter = new twitterApi({
