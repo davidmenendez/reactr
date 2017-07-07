18a944baf7f5a8c0f807660f5837d13acdef9883
David Menendez <damenend@gmail.com>
2017-07-07 12:18:36 -0500

    improved active nav check

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -6,10 +6,15 @@ class Sidebar extends React.Component {
     super(props);
     this.state = {};
     this.setPanel = this.setPanel.bind(this);
+    this.getNavClass = this.getNavClass.bind(this);
   }
 
   setPanel(e) {
     this.props.setPanel(e.target.textContent);
+  }
+
+  getNavClass(item) {
+    return this.props.activePanel === item ? 'nav--active' : '';
   }
 
   render() {
@@ -18,13 +23,13 @@ class Sidebar extends React.Component {
         <h1>REACTR</h1>
         <div className="sidebar-profile">
           <img src={this.props.user.profile_image_url} alt="profile" />
-          {this.props.user.name}
+          <span>{this.props.user.name}</span>
         </div>
         <nav className="sidebar-nav">
           <ul>
-            <li className={this.props.activePanel === 'geofeed' ? 'nav--active' : ''} onClick={this.setPanel} role="menuitem"><i className="fa fa-globe" />geofeed</li>
-            <li className={this.props.activePanel === 'followers' ? 'nav--active' : ''} onClick={this.setPanel} role="menuitem"><i className="fa fa-users" />followers</li>
-            <li className={this.props.activePanel === 'findFollowers' ? 'nav--active' : ''} onClick={this.setPanel} role="menuitem"><i className="fa fa-search" />findFollowers</li>
+            <li className={this.getNavClass('geofeed')} onClick={this.setPanel} role="menuitem"><i className="fa fa-globe" />geofeed</li>
+            <li className={this.getNavClass('followers')} onClick={this.setPanel} role="menuitem"><i className="fa fa-users" />followers</li>
+            <li className={this.getNavClass('findFollowers')} onClick={this.setPanel} role="menuitem"><i className="fa fa-search" />findFollowers</li>
             <li><a href="/logout"><i className="fa fa-sign-out" />log out</a></li>
           </ul>
         </nav>
